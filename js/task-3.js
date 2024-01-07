5cd58b4097e3057b5aa619f42f7c9dca8a94e7aa
Yriy40 <sirovyrij1986@gmail.com>
2024-01-07 21:33:14 +0200

    commit2

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,9 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = [...initialValue];
-  }
-  getValue() {
-    return this.#value.join("");
-  }
-  padEnd(str) {
-    this.#value.push(str);
-  }
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-  padStart(str) {
-    this.#value.unshift(str);
-  }
-  padBoth(str) {
-    this.#value.unshift(str);
-    this.#value.push(str);
-  }
+nameInput.addEventListener("input", updateGreeting);
+
+function updateGreeting() {
+  const enteredName = nameInput.value.trim();
+  nameOutput.textContent = enteredName || "Anonymous";
 }
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^=
