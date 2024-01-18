c5a2042fd51ccb0200afc959fc3b6c5fe0fea9c2
Yriy40 <sirovyrij1986@gmail.com>
2024-01-18 17:03:25 +0200

    commil6

---
+++ js/task-6.js
@@ -0,0 +1,37 @@
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+function createBoxes(amount) {
+  const initialSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = initialSize + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
